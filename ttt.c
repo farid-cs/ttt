c16f90c47c8d31c696333c19da4348241397dcaa
Farid <faridfaracli9@gmail.com>
2024-11-24 13:18:48 +0400

    simplify the code

--- ttt.c
+++ ttt.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 int board[3][3] = {
-	{'1', '2', '3'},
-	{'4', '5', '6'},
-	{'7', '8', '9'},
+	{'0', '1', '2'},
+	{'3', '4', '5'},
+	{'6', '7', '8'},
 };
 
 void
@@ -16,10 +16,9 @@ display_board(void)
 }
 
 void
-draw_on_board(int place, int symbol)
+draw_on_board(int cell, int symbol)
 {
-	place -= 1;
-	board[place/3][place%3] = symbol;
+	board[cell/3][cell%3] = symbol;
 }
 
 int
@@ -38,29 +37,53 @@ game_over(void)
 }
 
 int
+empty_cell(int cell)
+{
+	int symbol;
+
+	symbol = board[cell/3][cell%3];
+	return symbol != 'x' && symbol != 'o';
+}
+
+int
+take_turn(int player)
+{
+	int cell;
+
+	display_board();
+	printf("Player #%d's turn: ", player + 1);
+	fflush(stdout);
+
+	scanf("%d", &cell);
+	getchar();
+
+	if (cell < 0 || cell > 8) {
+		fprintf(stderr, "error: ivalid cell. Press enter to continue...");
+		getchar();
+		return -1;
+	}
+
+	if (!empty_cell(cell)) {
+		fprintf(stderr, "error: non empty cell. Press enter to continue...");
+		getchar();
+		return -1;
+	}
+
+	draw_on_board(cell, player % 2 ? 'o' : 'x');
+	return 0;
+}
+
+int
 main(void)
 {
 	int player = 0;
 
-	for (;;) {
-		int place;
-		int symbol = player % 2 ? 'o' : 'x';
-
-		display_board();
-		printf("Player #%d's turn: ", player + 1);
-		fflush(stdout);
-		scanf("%d", &place);
-		getchar();
-		if (place < 0 || place > 9) {
-			fprintf(stderr, "error: ivalid place. Press enter to continue...");
-			getchar();
+	while (!game_over()) {
+		if (take_turn(player))
 			continue;
-		}
-		draw_on_board(place, symbol);
-		if (game_over())
-			break;
-		player = (player + 1) % 2;
+		player ^= 1;
 	}
+
 	display_board();
 	printf("Player #%d won\n", player + 1);
 	return 0;
