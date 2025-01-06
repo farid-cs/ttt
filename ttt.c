f7783baa58d447a6a55d4f5b5bae6deaf842935b
Farid <faridfaracli9@gmail.com>
2025-01-06 16:21:44 +0400

    simplify code: represent board as array instead of matrix

--- ttt.c
+++ ttt.c
@@ -1,47 +1,36 @@
 #include <stdio.h>
 
-static int board[3][3] = {
-	{'0', '1', '2'},
-	{'3', '4', '5'},
-	{'6', '7', '8'},
+static int board[] = {
+	'0', '1', '2',
+	'3', '4', '5',
+	'6', '7', '8',
 };
 
 static void
 display_board(void)
 {
-	for (int i = 0; i != 3; i++) {
-		const int *row = board[i];
-		printf("%c %c %c\n", row[0], row[1], row[2]);
-	}
-}
-
-static void
-draw_on_board(int cell, int symbol)
-{
-	board[cell/3][cell%3] = symbol;
+	for (int i = 0; i < 9; i += 3)
+		printf("%c %c %c\n", board[i], board[i+1], board[i+2]);
 }
 
 static int
 game_over(void)
 {
 	return
-		board[0][0] == board[0][1] && board[0][1] == board[0][2] ||
-		board[1][0] == board[1][1] && board[1][1] == board[1][2] ||
-		board[2][0] == board[2][1] && board[2][1] == board[2][2] ||
-		board[0][0] == board[1][0] && board[1][0] == board[2][0] ||
-		board[0][1] == board[1][1] && board[1][1] == board[2][1] ||
-		board[0][2] == board[1][2] && board[1][2] == board[2][2] ||
-		board[0][0] == board[1][1] && board[1][1] == board[2][2] ||
-		board[0][2] == board[1][1] && board[1][1] == board[2][0]
+		board[0] == board[1] && board[1] == board[2] ||
+		board[3] == board[4] && board[4] == board[5] ||
+		board[6] == board[7] && board[7] == board[8] ||
+		board[0] == board[3] && board[3] == board[6] ||
+		board[1] == board[4] && board[4] == board[7] ||
+		board[2] == board[5] && board[5] == board[8] ||
+		board[0] == board[4] && board[4] == board[8] ||
+		board[2] == board[4] && board[4] == board[6]
 	;
 }
 
 static int
-empty_cell(int cell)
+isempty(int symbol)
 {
-	int symbol;
-
-	symbol = board[cell/3][cell%3];
 	return symbol != 'x' && symbol != 'o';
 }
 
@@ -58,18 +47,19 @@ take_turn(int player)
 	getchar();
 
 	if (cell < 0 || cell > 8) {
-		fprintf(stderr, "error: ivalid cell. Press enter to continue...");
+		fprintf(stderr, "error: invalid cell. Press enter to continue...");
 		getchar();
 		return -1;
 	}
 
-	if (!empty_cell(cell)) {
+	if (!isempty(board[cell])) {
 		fprintf(stderr, "error: non empty cell. Press enter to continue...");
 		getchar();
 		return -1;
 	}
 
-	draw_on_board(cell, player % 2 ? 'o' : 'x');
+	board[cell] = player % 2 ? 'o' : 'x';
+
 	return 0;
 }
 
@@ -78,13 +68,16 @@ main(void)
 {
 	int player = 0;
 
-	while (!game_over()) {
+	for (;;) {
 		if (take_turn(player))
 			continue;
+		if (game_over())
+			break;
 		player ^= 1;
 	}
 
 	display_board();
 	printf("Player #%d won\n", player + 1);
+
 	return 0;
 }
