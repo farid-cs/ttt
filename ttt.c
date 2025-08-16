0ba988b499d27de726a7427d6b40f564a3f9c5f1
Farid <faridfarajli86@gmail.com>
2025-08-17 01:32:36 +0400

    silence `-Wparentheses` warning

--- ttt.c
+++ ttt.c
@@ -11,15 +11,14 @@ static bool tie = false;
 static bool
 crossed(void)
 {
-	return
-		board[0] == board[1] && board[1] == board[2] ||
-		board[3] == board[4] && board[4] == board[5] ||
-		board[6] == board[7] && board[7] == board[8] ||
-		board[0] == board[3] && board[3] == board[6] ||
-		board[1] == board[4] && board[4] == board[7] ||
-		board[2] == board[5] && board[5] == board[8] ||
-		board[0] == board[4] && board[4] == board[8] ||
-		board[2] == board[4] && board[4] == board[6];
+	return (board[0] == board[1] && board[1] == board[2]) ||
+		(board[3] == board[4] && board[4] == board[5]) ||
+		(board[6] == board[7] && board[7] == board[8]) ||
+		(board[0] == board[3] && board[3] == board[6]) ||
+		(board[1] == board[4] && board[4] == board[7]) ||
+		(board[2] == board[5] && board[5] == board[8]) ||
+		(board[0] == board[4] && board[4] == board[8]) ||
+		(board[2] == board[4] && board[4] == board[6]);
 }
 
 static bool
